12b34c68661a37aad3d0e33f69ada844396410b7
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-03-09 19:58:57 +0000

    update redirection logic

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,28 +1,29 @@
 import { redirect, type Handle } from '@sveltejs/kit';
-import { setUser } from './lib/stores/auth';
 import { supabase } from '$lib/server/auth';
-import { getUserById } from '$lib/server/db/user';
+
+const isLandingPageRoute = (pathname: string) => {
+	return pathname === '/';
+};
+
+const getIsAuthenticatedRoute = (pathname: string) => {
+	return !['/login', '/signup', '/signup/verify-email', '/'].includes(pathname);
+};
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const accessToken = event.cookies.get('access_token');
+	const isAuthenticatedRoute = getIsAuthenticatedRoute(event.url.pathname);
+	const isLandingPage = isLandingPageRoute(event.url.pathname);
 
 	if (accessToken) {
 		const { data, error } = await supabase.auth.getUser(accessToken);
 
 		if (error) throw error;
 
-		if (data.user) {
-			const user = await getUserById(data.user.id);
-			setUser(user);
-
-			if (['/login', '/signup', '/signup/verify-email'].includes(event.url.pathname)) {
-				redirect(302, '/dashboard');
-			}
+		if (data.user && !isAuthenticatedRoute && !isLandingPage) {
+			redirect(302, '/dashboard');
 		}
-	} else {
-		if (!['/login', '/signup', '/', '/signup/verify-email'].includes(event.url.pathname)) {
-			redirect(302, '/login');
-		}
+	} else if (isAuthenticatedRoute) {
+		redirect(302, '/login');
 	}
 
 	return resolve(event);
