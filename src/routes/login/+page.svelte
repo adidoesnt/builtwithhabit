f8f48ab8a3c7434cfcda625cc312945c6da4a019
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-04-03 19:51:47 +0100

    add login page padding

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -52,7 +52,7 @@
 </script>
 
 <div
-	class="flex min-h-[100dvh] w-full flex-col items-center justify-center bg-cover bg-center md:bg-[url(/hut-with-mountains-1.jpg)]"
+	class="flex min-h-[100dvh] w-full flex-col items-center justify-center bg-cover bg-center md:bg-[url(/hut-with-mountains-1.jpg)] md:p-4"
 >
 	<div
 		id="container"
