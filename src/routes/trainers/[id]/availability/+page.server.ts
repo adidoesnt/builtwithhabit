d00457547747373a25572a1b48c662adbdca41a1
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-04-03 21:42:08 +0100

    Revert "fix redirects"
    This reverts commit 12afb4d56b1c58223471ef09066d7ed1c850a8e2.

--- src/routes/trainers/[id]/availability/+page.server.ts
+++ src/routes/trainers/[id]/availability/+page.server.ts
@@ -8,23 +8,23 @@ export const load = async ({ cookies }) => {
 	const accessToken = cookies.get('access_token');
 
 	if (!accessToken) {
-		throw redirect(303, '/login');
+		throw redirect(302, '/login');
 	}
 
 	const authUser = await supabase.auth.getUser(accessToken);
 
 	if (!authUser.data.user) {
-		throw redirect(303, '/login');
+		throw redirect(302, '/login');
 	}
 
 	const trainer = await getUserById(authUser.data.user.id);
 
 	if (!trainer) {
-		throw redirect(303, '/login');
+		throw redirect(302, '/login');
 	}
 
 	if (!trainer.roles.includes(Role.TRAINER)) {
-		throw redirect(303, '/');
+		throw redirect(302, '/');
 	}
 
 	const availability = await getTrainerAvailability();
