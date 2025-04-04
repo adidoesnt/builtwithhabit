16970f418132dff5b0a07c52c724c8a370c61ee3
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-04-04 15:44:59 +0100

    update init script

--- terraform/init.sh
+++ terraform/init.sh
@@ -1,8 +1,8 @@
 source .envrc
 
 # Create TF State S3 Bucket
-aws s3api create-bucket --bucket bwh-tf-state --region ap-southeast-1 --create-bucket-configuration LocationConstraint=ap-southeast-1
-aws s3api put-bucket-versioning --bucket bwh-tf-state --versioning-configuration Status=Enabled
+aws s3api create-bucket --bucket bwh-tf-state --region ap-southeast-1 --create-bucket-configuration LocationConstraint=ap-southeast-1 >> /dev/null 2>&1
+aws s3api put-bucket-versioning --bucket bwh-tf-state --versioning-configuration Status=Enabled >> /dev/null 2>&1
 
 # Create TF State Lock DynamoDB Table
 aws dynamodb create-table \
@@ -10,4 +10,4 @@ aws dynamodb create-table \
     --attribute-definitions AttributeName=LockID,AttributeType=S \
     --key-schema AttributeName=LockID,KeyType=HASH \
     --billing-mode PAY_PER_REQUEST \
-    --region ap-southeast-1
+    --region ap-southeast-1 >> /dev/null 2>&1
