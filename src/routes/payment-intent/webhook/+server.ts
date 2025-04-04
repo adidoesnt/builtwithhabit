df0603c6f2cc81bbe8748c8c0fb0604d8d2742b4
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-04-04 19:01:37 +0100

    add sqs send message

--- src/routes/payment-intent/webhook/+server.ts
+++ src/routes/payment-intent/webhook/+server.ts
@@ -4,6 +4,7 @@ import { json } from '@sveltejs/kit';
 import { WebhookEvent, PurchaseStatus } from '../types';
 import stripe from '$lib/server/stripe';
 import { env } from '$env/dynamic/private';
+import { sendMessage } from '$lib/server/sqs';
 
 const { STRIPE_WEBHOOK_SECRET } = env;
 
@@ -33,7 +34,8 @@ export const POST = async ({ request }) => {
 		let paymentIntent: PaymentIntent | null = null;
 		switch (event.type) {
 			case WebhookEvent.PaymentIntentCreated:
-				// TODO: Add logic to check if the payment intent has succeeded, if not, cancel and delete the purchase
+				paymentIntent = event.data.object as PaymentIntent;
+				await sendMessage({ payment_intent_id: paymentIntent.id });
 				break;
 			case WebhookEvent.PaymentIntentSucceeded:
 			case WebhookEvent.ChargeSucceeded:
