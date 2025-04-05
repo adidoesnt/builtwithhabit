8f81bf0336e853121016f0c4e7da9494e3e735a6
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-04-05 15:54:14 +0100

    add show password toggle for login and signup pages

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -17,6 +17,7 @@
 	});
 
 	let isLoading = $state(false);
+	let showPassword = $state(false);
 
 	$effect(() => {
 		if (formValues.email && form?.errors?.email && formValues.email !== form.data?.email) {
@@ -47,6 +48,10 @@
 			};
 		}
 	});
+
+	function togglePasswordVisibility() {
+		showPassword = !showPassword;
+	}
 </script>
 
 <div
@@ -88,13 +93,58 @@
 				</div>
 				<div class="mb-4 flex flex-col gap-2">
 					<label for="password">Password</label>
-					<input
-						class="rounded-sm border-none p-2"
-						type="password"
-						name="password"
-						placeholder="Password"
-						bind:value={formValues.password}
-					/>
+					<div class="relative">
+						<input
+							class="w-full rounded-sm border-none p-2 pr-10"
+							type={showPassword ? 'text' : 'password'}
+							name="password"
+							placeholder="Password"
+							bind:value={formValues.password}
+						/>
+						<button
+							type="button"
+							class="hover:text-dark-brown absolute top-1/2 right-2 -translate-y-1/2 text-gray-500"
+							onclick={togglePasswordVisibility}
+							aria-label={showPassword ? 'Hide password' : 'Show password'}
+						>
+							{#if showPassword}
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									class="h-5 w-5"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+									/>
+								</svg>
+							{:else}
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									class="h-5 w-5"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+									/>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+									/>
+								</svg>
+							{/if}
+						</button>
+					</div>
 					{#if form?.errors?.password}
 						<p class="text-sm text-red-400">{form.errors.password}</p>
 					{/if}
