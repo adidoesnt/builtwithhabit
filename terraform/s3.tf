074d6af4bac85e9cfd2648c12114cdf8cfe1bf33
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-04-10 22:02:14 +0100

    create dev bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,15 +1,55 @@
+# Production
 resource "aws_s3_bucket" "bwh_main_bucket" {
-    bucket = "bwh-main-bucket"
+  bucket = "bwh-main-bucket"
 
-    tags = {
-        Name = "BWH Main Bucket"
-    }
+  tags = {
+    Name = "BWH Main Bucket"
+  }
 }
 
 resource "aws_s3_bucket_versioning" "bwh_main_bucket_versioning" {
-    bucket = aws_s3_bucket.bwh_main_bucket.id
+  bucket = aws_s3_bucket.bwh_main_bucket.id
 
-    versioning_configuration {
-        status = "Enabled"
-    }
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_cors_configuration" "bwh_main_bucket_cors" {
+  bucket = aws_s3_bucket.bwh_main_bucket.id
+
+  cors_rule {
+    allowed_methods = ["GET", "PUT", "POST", "DELETE"]
+    allowed_origins = ["*.beingsofhabit.com", "*.boh-services.com"]
+    allowed_headers = ["Content-Type"]
+    max_age_seconds = 3000
+  }
+}
+
+# Staging
+resource "aws_s3_bucket" "bwh_staging_bucket" {
+  bucket = "bwh-staging-bucket"
+
+  tags = {
+    Name = "BWH Staging Bucket"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "bwh_staging_bucket_versioning" {
+  bucket = aws_s3_bucket.bwh_staging_bucket.id
+
+  versioning_configuration {
+    status = "Disabled"
+  }
+}
+
+resource "aws_s3_bucket_cors_configuration" "bwh_staging_bucket_cors" {
+  bucket = aws_s3_bucket.bwh_staging_bucket.id
+
+  cors_rule {
+    allowed_methods = ["GET", "PUT", "POST", "DELETE"]
+    allowed_origins = ["*"]
+    allowed_headers = ["*"]
+    max_age_seconds = 3000
+  }
 }
