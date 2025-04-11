6ef4899b4c23e35d94fe73eab715c423f749df2b
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-04-11 21:45:30 +0100

    add fetch user profile picture

---
+++ src/routes/profile/+page.server.ts
@@ -0,0 +1,59 @@
+import { supabase } from '$lib/server/auth';
+import { getUserById } from '$lib/server/db/user';
+import {
+	getPresignedUrlForUpload,
+	getPresignedUrlForViewOrDownload,
+	listFilesInDir
+} from '$lib/server/s3/index.js';
+import { UserDir } from '$lib/types.js';
+import { redirect } from '@sveltejs/kit';
+
+export const load = async ({ cookies }) => {
+	const accessToken = cookies.get('access_token');
+
+	if (!accessToken) {
+		console.log('No access token');
+		throw redirect(303, '/login');
+	}
+
+	const authUser = await supabase.auth.getUser(accessToken);
+
+	if (!authUser.data.user) {
+		console.log('User not found', authUser.data.user);
+		throw redirect(303, '/login');
+	}
+
+	const user = await getUserById(authUser.data.user.id);
+
+	if (!user) {
+		console.log('User not found', authUser.data.user.id);
+		throw redirect(303, '/login');
+	}
+
+	const profilePictureDirFiles = await listFilesInDir(user.id, UserDir.PROFILE_PICTURE);
+	const profilePictureFile = profilePictureDirFiles[0];
+
+	const { url: profilePictureViewUrl, deleteUrl: profilePictureDeleteUrl } =
+		profilePictureFile ?? {};
+
+	let hasProfilePicture = false;
+
+	if (profilePictureViewUrl) {
+		const profilePictureGetResponse = await fetch(profilePictureViewUrl);
+		hasProfilePicture = profilePictureGetResponse.ok;
+	}
+
+	const profilePictureUploadUrl = await getPresignedUrlForUpload(
+		user.id,
+		UserDir.PROFILE_PICTURE,
+		profilePictureFile?.name
+	);
+
+	return {
+		user,
+		profilePictureViewUrl,
+		profilePictureDeleteUrl,
+		profilePictureUploadUrl,
+		hasProfilePicture
+	};
+};
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -3,6 +3,15 @@
 	import LogoHeader from '$lib/components/LogoHeader.svelte';
 	import LoadingSpinner from '$lib/components/LoadingSpinner.svelte';
 	import { goto } from '$app/navigation';
+	import type { PageData } from './$types';
+
+	const { data }: { data: PageData } = $props();
+	const {
+		profilePictureViewUrl,
+		profilePictureDeleteUrl,
+		profilePictureUploadUrl,
+		hasProfilePicture
+	} = data;
 
 	let isLoggingOut = $state(false);
 
@@ -42,9 +51,18 @@
 						{$user.firstName[0]}{$user.lastName[0]}
 					</div>
 					<div class="text-center md:text-left">
-						<h2 class="font-body text-olive text-2xl font-semibold">
-							{formatName($user.firstName, $user.lastName)}
-						</h2>
+						{#if hasProfilePicture}
+							<!-- svelte-ignore a11y_img_redundant_alt -->
+							<img
+								src={profilePictureViewUrl}
+								alt="Profile Picture"
+								class="h-24 w-24 rounded-full"
+							/>
+						{:else}
+							<h2 class="font-body text-olive text-2xl font-semibold">
+								{formatName($user.firstName, $user.lastName)}
+							</h2>
+						{/if}
 						<p class="font-body text-olive">{$user.email}</p>
 					</div>
 				</div>
