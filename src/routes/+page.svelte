020f153c3301f0b5afac481321496a9e14360583
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-07-19 20:11:39 +0100

    add about page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { Logo } from '$lib/icons';
 	import config from '$lib/config';
+	import { Mission } from '$lib/components';
 </script>
 
 <div class="flex h-fit w-full flex-col">
@@ -40,42 +41,7 @@
 	</section>
 
 	<!-- Mission section -->
-	<section
-		id="mission-section"
-		class="text-dark-brown flex min-h-[calc(100dvh-9rem)] flex-col place-items-center gap-8 bg-white p-16"
-	>
-		<h1 class="font-headings text-dark-brown text-5xl font-bold tracking-wide">
-			our brand mission
-		</h1>
-		<div class="grid grid-cols-[2fr_1fr] gap-8 text-left">
-			<div id="mission-text" class="flex flex-col justify-center gap-4">
-				<p class="font-body text-dark-brown text-2xl">
-					sweat smarter. <span class="font-bold">live stronger.</span>
-				</p>
-				<p class="font-body text-dark-brown text-2xl">
-					at builtwithhabit, we blend science-backed coaching and skin-safe activewear to help you
-					train smarter, feel stronger, and build habits that last.
-				</p>
-				<p class="font-body text-dark-brown text-2xl">
-					on the coaching side, we root things in the basics: resistance training, whole-food
-					nutrition, and sustainable structure, personalised to you. smart, science-backed progress.
-				</p>
-				<p class="font-body text-dark-brown text-2xl">
-					on the activewear front, we’re rethinking apparel from the fiber up. no polyester, no
-					plastic-based coatings, no harmful dyes, no greenwashed gimmicks - just skin-safe fabrics
-					engineered for real performance. from glute-sculpting leggings to high-support bras, our
-					pieces are seamless, sweat-wicking, and intentionally-built for intense motion.
-				</p>
-			</div>
-			<div id="mission-image" class="flex items-center justify-center overflow-hidden rounded-sm">
-				<img
-					src="/headshot.jpg"
-					alt="Headshot of the founder"
-					class="h-full w-full rounded-sm object-cover"
-				/>
-			</div>
-		</div>
-	</section>
+	<Mission />
 
 	<!-- Pillars section -->
 	<section
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import { Mission } from '$lib/components';
+</script>
+
+<div class="flex h-fit w-full flex-col">
+	<section id="hero-section" class="bg-white flex min-h-[calc(100dvh-12rem)] w-full flex-col">
+		<div class="flex w-full flex-col items-center justify-center p-8 gap-8">
+			<h1 class="font-headings text-dark-brown text-5xl font-bold tracking-wide">
+                our why
+            </h1>
+			<div class="grid grid-cols-[1fr_2fr] gap-8 text-left">
+				<div id="why-image" class="flex items-center justify-center overflow-hidden rounded-sm flex-col gap-4">
+					<img
+						src="/training-image-1.jpg"
+						alt="Founder at the beach"
+						class="h-full w-full rounded-sm object-cover aspect-[8/9]"
+					/>
+                    <!-- TODO: Find the correct HREF -->
+                    <a href="/" class="font-body bg-light-brown text-beige p-2 px-4 rounded-md text-lg hover:opacity-70 transition-all duration-300">
+                        learn more about my journey!
+                    </a>
+				</div>
+				<div id="why-text" class="flex flex-col justify-center gap-4">
+					<p class="font-body text-dark-brown text-2xl">
+						hi, i’m ishita, a NASM-certified trainer with a deep love for movement and a passion for
+						helping women build strength they can actually feel.
+					</p>
+					<p class="font-body text-dark-brown text-2xl">
+						as i’ve grown older, my relationship with fitness has evolved from rigid routines to
+						something more intuitive and empowering. lifting has become my anchor; a way to feel
+						clear, consistent, and at home in my body: and i wanted others to experience this too.
+					</p>
+					<p class="font-body text-dark-brown text-2xl">
+						as i coached more women and dove deeper into the science, though, i realised something
+						was still missing; not just in how we train, but what we train in. that’s where the
+						activewear came in. builtwithhabit was born out of a desire to create gear that’s as
+						intentional as the training itself: high-performance, skin-safe, and free from the
+						hidden plastics and toxins found in most athletic wear.
+					</p>
+					<p class="font-body text-dark-brown text-2xl">
+						today, i blend that lived experience with science-backed coaching and
+						thoughtfully-engineered apparel: to support your habits, from the inside out.
+					</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+    <!-- Mission section -->
+    <Mission background="bg-beige" />
+</div>
