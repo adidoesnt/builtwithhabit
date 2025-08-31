97def1fcd89dd69f259a52f236545d9266aa9172
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-08-31 18:01:14 +0100

    update components exports

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,3 +1,6 @@
 export { default as Mission } from './Mission.svelte';
 export { default as LoadingSpinner } from './LoadingSpinner.svelte';
 export { Packages, PackagesTable, PackagesAccordion } from './Packages';
+export { default as Analytics } from './Analytics.svelte';
+export { default as Footer } from './Footer.svelte';
+export { default as Header } from './Header.svelte';
